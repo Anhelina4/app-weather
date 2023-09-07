e5d75abc44b534193ac63ae20cfc0c4a4267a734
Anhelina Bohdanova <gelyabogdanova88@gmail.com>
2023-09-07 11:02:57 +0300

    [LAYOUT] Started working, addded appLayout

--- src/components/bundles/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <h1 className="text-3xl font-bold underline hover:ring-indigo-500 text-red-600">
-      Hello world!
-    </h1>
-  )
-}
-
-export default Header
