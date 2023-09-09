e3d269a09547b01f4725b048df8136aaf2492a62
Anhelina Bohdanova <gelyabogdanova88@gmail.com>
2023-09-09 12:10:12 +0300

    Finish app layout

--- src/components/layouts/AppLayout/AppLayout.tsx
+++ src/components/layouts/AppLayout/AppLayout.tsx
@@ -158,16 +158,16 @@ const AppLayout = (props: Props) => {
           )}
         </Disclosure>
 
-        <header className="bg-white shadow">
+        {/* <header className="bg-white shadow">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold tracking-tight text-gray-900">
               Dashboard
             </h1>
           </div>
-        </header>
+        </header> */}
         <main>
           <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            {/* Your content */}
+            {children}
           </div>
         </main>
       </div>
